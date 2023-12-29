5d4608a0a924e5a0b835b1c7661f1fab07d22223
iceprins <junghankyul@gmail.com>
2023-12-29 11:08:13 +0900

    feat: baekjoon 13305 solve

--- baekjoon/13305.py
+++ baekjoon/13305.py
@@ -1,16 +1,15 @@
 import sys
 
 if __name__ == '__main__':
-    city = int(input())
-    road = list(map(int, sys.stdin.readline().strip().split()))
-    petrol = list(map(int, sys.stdin.readline().strip().split()))
+    N = int(sys.stdin.readline().strip())
+    load_length = list(map(int, sys.stdin.readline().strip().split()))
+    oil_price = list(map(int, sys.stdin.readline().strip().split()))
 
-    final_cost = 0
+    ans = 0
+    min_price = oil_price[0]
+    for i in range(1, N):
+        ans += min_price * load_length[i - 1]
+        if oil_price[i] <= min_price:
+            min_price = oil_price[i]
 
-    min_cost = petrol[0]
-    for i in range(city - 1):
-        if petrol[i] < min_cost:
-            min_cost = petrol[i]
-        final_cost += min_cost * road[i]
-
-    print(final_cost)
+    print(ans)
