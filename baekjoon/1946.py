0b9c4ee5a5a48e8a236c13f4504050f113f55694
iceprins <junghankyul@gmail.com>
2024-01-03 13:03:30 +0900

    refoctor: baekjoon 1946 re-solve

--- baekjoon/1946.py
+++ baekjoon/1946.py
@@ -1,22 +1,23 @@
 import sys
 
-case_num = int(sys.stdin.readline().strip())
+if __name__ == '__main__':
+    T = int(sys.stdin.readline().strip())
 
-for _ in range(case_num):
-    A = list()
-    result = 1
-    cand_num = int(sys.stdin.readline().strip())
-    for _ in range(cand_num):
-        paper_score, interview_score = map(int, sys.stdin.readline().split())
-        A.append((paper_score, interview_score))
+    for _ in range(T):
+        N = int(sys.stdin.readline().strip())
+        applicants = list()
+        for _ in range(N):
+            applicants.append(tuple(map(int, sys.stdin.readline().strip().split())))
 
-    A.sort(key=lambda x: x[0])
+        applicants.sort(reverse=True, key=lambda x: x[1])
+        applicants.sort(key=lambda x:x[0])
 
-    temp = A[0][1]
+        cnt = 1
+        std_score = applicants[0][1]
 
-    for i in range(1, cand_num):
-        if A[i][1] < temp:
-            result += 1
-            temp = A[i][1]
+        for i in range(1, N):
+            if applicants[i][1] <= std_score:
+                cnt += 1
+                std_score = applicants[i][1]
 
-    print(result)
+        print(cnt)
