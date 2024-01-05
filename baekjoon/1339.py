d2ef710725771104cc525066876d01091d6dc400
iceprins <junghankyul@gmail.com>
2024-01-05 15:52:29 +0900

    feat: baekjoon 1339 solve

---
+++ baekjoon/1339.py
@@ -0,0 +1,25 @@
+import sys
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline().strip())
+    words = list()
+    calculate = dict()
+    num = [i for i in range(0, 10)]
+    ans = 0
+
+    for _ in range(N):
+        word = sys.stdin.readline().strip()
+        words.append(word)
+        tmp = 0
+        for i in range(len(word), 0, -1):
+            if word[-i] not in calculate:
+                calculate[word[-i]] = 10 ** (i - 1)
+            else:
+                calculate[word[-i]] += 10 ** (i - 1)
+
+    calculate = sorted(calculate.values(), reverse=True)
+
+    for elem in calculate:
+        ans += elem * num.pop()
+
+    print(ans)
