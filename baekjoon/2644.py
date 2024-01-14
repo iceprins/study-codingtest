05d5374a1fa683c3a916c85cbea8d11b3bb36eb4
iceprins <junghankyul@gmail.com>
2024-01-14 11:54:04 +0900

    refactor: baekjoon 2644 modify

--- baekjoon/2644.py
+++ baekjoon/2644.py
@@ -1,25 +1,15 @@
 import sys
-from collections import deque
 
-# def dfs(g, start, v):
-
-
-def bfs(g, start, v):
-    result = -1
-    queue = deque()
-    queue.append(start)
+def dfs(g, start, v, cnt):
+    cnt += 1
     v[start] = True
-    while queue:
-        now = queue.popleft()
-        for target in g[now]:
-            if second == target:
-                return result
-            if not v[target]:
-                queue.append(target)
-                v[target] = True
-                result += 1
 
-    return -1
+    if start == second:
+        ans.append(cnt)
+
+    for i in g[start]:
+        if not v[i]:
+            dfs(g, i, v, cnt)
 
 if __name__ == '__main__':
     n = int(sys.stdin.readline().strip())
@@ -28,11 +18,16 @@ if __name__ == '__main__':
 
     edges = [[] for _ in range(n + 1)]
     visited = [False] * (n + 1)
+    ans = list()
 
     for i in range(m):
         a, b = map(int, sys.stdin.readline().strip().split())
         edges[a].append(b)
-        # edges[b].append(a)
+        edges[b].append(a)
 
-    print(bfs(edges, first, visited))
-    print(edges)
+    dfs(edges, first, visited, 0)
+
+    if len(ans) == 0:
+        print(-1)
+    else:
+        print(ans[0] - 1)
