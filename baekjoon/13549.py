ec0d810d0a1407aa659273ab5fa80f0eef38b682
iceprins <junghankyul@gmail.com>
2024-01-22 02:22:09 +0900

    refactor: baekjoon 13549 re-solve

--- baekjoon/13549.py
+++ baekjoon/13549.py
@@ -1,37 +1,30 @@
-import sys
 from collections import deque
 
 
-me, sister = map(int, sys.stdin.readline().strip().split())
-max_dist = 100001
-check = [False] * max_dist
-dist = [-1] * max_dist
-queue = deque()
+def bfs(s, v, h):
+    queue = deque()
+    queue.append(s)
+    v[s] = True
 
-queue.append(me)
-check[me] = True
-dist[me] = 0
-
-
-def bfs():
     while queue:
         now = queue.popleft()
-        if now == sister:
-            print(dist[now])
-            break
-        if 0 <= now - 1 < max_dist and check[now - 1] == False:
-            queue.append(now - 1)
-            check[now - 1] = True
-            dist[now - 1] = dist[now] + 1
-        if 0 < now * 2 < max_dist and check[now * 2] == False:
-            queue.appendleft(now * 2)
-            check[now * 2] = True
-            dist[now * 2] = dist[now]
-        if 0 <= now + 1 < max_dist and check[now + 1] == False:
-            queue.append(now + 1)
-            check[now + 1] = True
-            dist[now + 1] = dist[now] + 1
+        if now == K:
+            return h[now]
+        for new in (2 * now, now - 1, now + 1):
+            if new < 0 or new > 100000:
+                continue
+            if not v[new]:
+                v[new] = True
+                queue.append(new)
+                if new == 2 * now:
+                    h[new] = h[now]
+                else:
+                    h[new] = h[now] + 1
 
 
 if __name__ == '__main__':
-    bfs()
+    N, K = map(int, input().strip().split())
+    visited = [False] * 100001
+    history = [0] * 100001
+
+    print(bfs(N, visited, history))
