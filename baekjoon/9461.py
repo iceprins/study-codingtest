d059b4bb926aeca74bb1efef76eee22ecff1806f
iceprins <junghankyul@gmail.com>
2024-02-10 17:42:05 +0900

    feat: baekjoon 9461 solve

---
+++ baekjoon/9461.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == '__main__':
+    T = int(sys.stdin.readline().strip())
+
+    for _ in range(T):
+        N = int(sys.stdin.readline().strip())
+        dp = [0] * (N + 1)
+
+        for i in range(1, N + 1):
+            if i == 1:
+                dp[i] = 1
+            elif i == 2:
+                dp[i] = 1
+            else:
+                dp[i] = dp[i - 2] + dp[i - 3]
+
+        print(dp[N])
