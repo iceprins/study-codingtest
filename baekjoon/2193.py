ab76502de923243eb3f595acb81254aa31a785f6
iceprins <junghankyul@gmail.com>
2024-02-11 11:21:43 +0900

    feat: baekjoon 2193 solve

---
+++ baekjoon/2193.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    N = int(input())
+    dp = [0] * (N + 1)
+
+    for i in range(1, N + 1):
+        if i == 1:
+            dp[i] = 1
+        elif i == 2:
+            dp[i] = 1
+        else:
+            dp[i] = dp[i - 2] + dp[i - 1]
+
+    print(dp[N])
