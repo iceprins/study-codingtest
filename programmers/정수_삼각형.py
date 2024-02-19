b2d9e0fd590872224e079c3c9edf8aff6531f93a
iceprins <junghankyul@gmail.com>
2024-02-19 15:10:03 +0900

    feat: programmers 정수_삼각형 solve

---
+++ programmers/정수_삼각형.py
@@ -0,0 +1,17 @@
+def solution(triangle):
+    for i in range(len(triangle) - 1):
+        for j in range(len(triangle[i + 1])):
+            if i == 0:
+                triangle[1][j] += triangle[0][0]
+            else:
+                if j == 0:
+                    triangle[i + 1][0] += triangle[i][0]
+                elif j == len(triangle[i + 1]) - 1:
+                    triangle[i + 1][j] += triangle[i][len(triangle[i]) - 1]
+                else:
+                    triangle[i + 1][j] = max(triangle[i + 1][j] + triangle[i][j - 1],
+                                             triangle[i + 1][j] + triangle[i][j])
+
+    answer = max(triangle[-1])
+
+    return answer
