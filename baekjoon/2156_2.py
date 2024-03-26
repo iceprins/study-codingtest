bd9956aa4ce3f26d02abad83177e4f03eda8e2c2
iceprins <junghankyul@gmail.com>
2024-03-26 16:19:38 +0900

    feat: baekjoon 2156 re-solve

---
+++ baekjoon/2156_2.py
@@ -0,0 +1,19 @@
+import sys
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline().strip())
+    wines = [int(sys.stdin.readline().strip()) for _ in range(n)]
+
+    dp = [0] * n
+
+    for i in range(0, n):
+        if i == 0:
+            dp[i] = wines[0]
+        elif i == 1:
+            dp[i] = wines[0] + wines[1]
+        elif i == 2:
+            dp[i] = max(wines[0] + wines[1], wines[0] + wines[2], wines[1] + wines[2])
+        else:
+            dp[i] = max(dp[i - 2] + wines[i], dp[i - 3] + wines[i - 1] + wines[i], dp[i - 1])
+
+    print(dp[n - 1])
