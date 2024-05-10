89e8dc3d4936e932f25b799785e9dd68427645e5
iceprins <junghankyul@gmail.com>
2024-05-10 11:58:13 +0900

    modify: add another solution of 1043

--- baekjoon/1043.py
+++ baekjoon/1043.py
@@ -1,3 +1,4 @@
+# set 풀이
 import sys
 
 if __name__ == '__main__':
@@ -18,6 +19,62 @@ if __name__ == '__main__':
 
     for party in parties:
         if len(party.intersection(true)) == 0:
+            ans += 1
+
+    print(ans)
+
+
+# union-find 풀이
+import sys
+
+
+def find(x):
+    if parents[x] != x:
+        parents[x] = find(parents[x])
+    return parents[x]
+
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x in true and y in true:
+        return
+
+    if x in true:
+        parents[y] = x
+    elif y in true:
+        parents[x] = y
+    else:
+        if x < y:
+            parents[y] = x
+        else:
+            parents[x] = y
+
+
+if __name__ == '__main__':
+    N, M = map(int, sys.stdin.readline().strip().split())
+    true = list(map(int, sys.stdin.readline().strip().split()))[1:]
+    parties = []
+    parents = [i for i in range(N + 1)]
+
+    for _ in range(M):
+        party = list(map(int, sys.stdin.readline().strip().split()))
+        party_len = party[0]
+        party = party[1:]
+
+        for i in range(party_len - 1):
+            union(party[i], party[i + 1])
+
+        parties.append(party)
+
+    ans = 0
+
+    for party in parties:
+        for i in range(len(party)):
+            if find(party[i]) in true:
+                break
+        else:
             ans += 1
 
     print(ans)
