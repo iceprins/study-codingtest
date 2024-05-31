4ebdeac190dc3b719855698a3c19abbfac1d130f
iceprins <junghankyul@gmail.com>
2024-05-31 10:42:10 +0900

    modify: update 1719

--- baekjoon/1719.py
+++ baekjoon/1719.py
@@ -1,50 +1,50 @@
 # Dijkstra 풀이
-# import sys
-# import heapq
-#
-#
-# def dijkstra(start):
-#     q = []
-#     heapq.heappush(q, (0, start))
-#     distance = [sys.maxsize] * (n + 1)
-#     distance[start] = 0
-#     path[start] = [start]
-#
-#     while q:
-#         dist, now = heapq.heappop(q)
-#
-#         if distance[now] < dist:
-#             continue
-#
-#         for i in graph[now]:
-#             if dist + i[1] < distance[i[0]]:
-#                 distance[i[0]] = dist + i[1]
-#                 heapq.heappush(q, (dist + i[1], i[0]))
-#                 path[i[0]] = path[now] + [i[0]]
-#
-#     ans = []
-#
-#     for i in range(1, n + 1):
-#         if i == start:
-#             ans.append('-')
-#         else:
-#             ans.append(path[i][1])
-#
-#     return ans
-#
-#
-# if __name__ == '__main__':
-#     n, m = map(int, sys.stdin.readline().strip().split())
-#     graph = [[] for _ in range(n + 1)]
-#     path = [['-' for _ in range(n + 1)] for _ in range(n + 1)]
-#
-#     for _ in range(m):
-#         a, b, c = map(int, sys.stdin.readline().strip().split())
-#         graph[a].append((b, c))
-#         graph[b].append((a, c))
-#
-#     for i in range(1, n + 1):
-#         print(*dijkstra(i))
+import sys
+import heapq
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance = [sys.maxsize] * (n + 1)
+    distance[start] = 0
+    path[start] = [start]
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            if dist + i[1] < distance[i[0]]:
+                distance[i[0]] = dist + i[1]
+                heapq.heappush(q, (dist + i[1], i[0]))
+                path[i[0]] = path[now] + [i[0]]
+
+    ans = []
+
+    for i in range(1, n + 1):
+        if i == start:
+            ans.append('-')
+        else:
+            ans.append(path[i][1])
+
+    return ans
+
+
+if __name__ == '__main__':
+    n, m = map(int, sys.stdin.readline().strip().split())
+    graph = [[] for _ in range(n + 1)]
+    path = [['-' for _ in range(n + 1)] for _ in range(n + 1)]
+
+    for _ in range(m):
+        a, b, c = map(int, sys.stdin.readline().strip().split())
+        graph[a].append((b, c))
+        graph[b].append((a, c))
+
+    for i in range(1, n + 1):
+        print(*dijkstra(i))
 
 
 # Floyd_Warshall 풀이
