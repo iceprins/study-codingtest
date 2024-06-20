faeb3da11a53f5bc436352d9cad899383be14bdb
iceprins <junghankyul@gmail.com>
2024-06-20 14:46:36 +0900

    feat: baekjoon 1654 re-solve

--- baekjoon/1654.py
+++ baekjoon/1654.py
@@ -1,35 +1,21 @@
 import sys
 
-
-def calculate(n):
-    result = 0
-    for line in lines:
-        if line >= n:
-            result += line // n
-
-    return result
-
-
-def search(left, right, target):
-    while left <= right:
-        mid = (left + right) // 2
-        if calculate(mid) == target:
-            return mid
-        elif calculate(mid) > target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return right
-
-
 if __name__ == '__main__':
     K, N = map(int, sys.stdin.readline().strip().split())
-    lines = list()
+    lines = sorted([int(sys.stdin.readline().strip()) for _ in range(K)])
 
-    for _ in range(K):
-        lines.append(int(sys.stdin.readline().strip()))
+    start, end = 1, lines[-1]
 
-    lines.sort()
+    while start <= end:
+        mid = (start + end) // 2
+        n = 0
 
-    print(search(0, lines[-1], N))
+        for line in lines:
+            n += line // mid
+
+        if n >= N:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    print(end)
