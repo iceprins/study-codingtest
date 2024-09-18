8879c572363c120948b3fad2e4679687ea74d82a
iceprins <junghankyul@gmail.com>
2024-09-18 12:29:14 +0900

    refactor: 다른 풀이 추가

--- programmers/뒤에_있는_큰_수_찾기.py
+++ programmers/뒤에_있는_큰_수_찾기.py
@@ -1,3 +1,18 @@
+# 단순 구현 풀이
+def solution(numbers):
+    answer = [-1] * len(numbers)
+    
+    for i in range(len(numbers) - 1, 0, -1):
+        num = numbers[i]
+        for j in range(i - 1, -1, -1):
+            if numbers[j] >= num:
+                break
+            answer[j] = num
+
+    return answer
+
+
+# Stack 풀이
 def solution(numbers):
     answer = [-1] * len(numbers)
     st = []
